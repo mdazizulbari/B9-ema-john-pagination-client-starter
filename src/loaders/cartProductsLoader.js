e4d97dcb760dc3511d03201bf6007ca61e2007c4
dmabari <mdazizulbarishafin@gmail.com>
2025-07-01 20:31:00 +0600

    fsfwf

--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,14 +1,18 @@
 import { getShoppingCart } from "../utilities/fakedb";
 
 const cartProductsLoader = async () => {
-  const loadedProducts = await fetch("http://localhost:5000/products");
-  const products = await loadedProducts.json();
-
   // if cart data is in database, you have to use async await
   const storedCart = getShoppingCart();
   const storedCartIds = Object.keys(storedCart);
-  console.log({ storedCart });
   const savedCart = [];
+  console.log(storedCartIds);
+
+  const loadedProducts = await fetch("http://localhost:5000/productsByIds", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(storedCartIds),
+  });
+  const products = await loadedProducts.json();
 
   for (const id in storedCart) {
     const addedProduct = products.find((pd) => pd._id === id);
